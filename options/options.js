ccecf0dfbe09f2621934ea325827d0b41cdf2383
Crowdbotics <team@crowdbotics.com>
2024-02-05 15:50:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "airbus-route-planni-45910",
+  url: "https://airbus-route-planni-45910.botics.co",
+  api: "https://airbus-route-planni-45910.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
